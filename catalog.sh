ba8eacb823e8d16cd2ca81c0aff935d14fd32bc2
srbhm <106704404+srbhm@users.noreply.github.com>
2022-06-02 02:37:06 -0700

    http from the terminal - project 1 solution

---
+++ catalog.sh
@@ -0,0 +1,10 @@
+# References:
+# 1. https://httpie.io/docs/cli/querystring-parameters
+# 2. https://httpie.io/docs/cli/escaping-behavior
+course="${1}"
+tmp_file="catalog_search_data.html"
+http GET https://catalog.fullerton.edu/search_advanced.php \
+	cur_cat_oid==75 search_database==Search search_db==Search cpage==1 ecpage==1 ppage==1 spage==1 \
+	tpage==1 location==33 filter\[keyword\]=="CPSC $course" filter\[exact_match\]==1 > $tmp_file
+cat $tmp_file | grep "Best Match:"
+rm $tmp_file
