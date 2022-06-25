2c590210d600fc933470c4d8f1b22589c4559446
saurami <106704404+saurami@users.noreply.github.com>
2022-06-24 21:09:15 -0700

    final edits for value comparison

--- cookies.sh
+++ cookies.sh
@@ -10,10 +10,10 @@ http "httpbin.org/cookies/set?page=3&user=bar" --session=./bar.json > /dev/null
 bar_page=$(jq --raw-output '.cookies[] | select(.name=="page") | .value' ./bar.json)
 
 foobar1(){
-	if [ "$foo_page_updated" = "$bar_page" ]; then
-		echo "[FAILED] Values should not be the same ..."
+	if [[ "$foo_page_updated" == 2 && "$bar_page" == 3 ]]; then
+		echo "[PASSED] Correct values of updated foo and initial bar"
 	else
-		echo "[PASSED] Values of updated foo and initial bar are different"
+		echo "[FAILED] Incorrect values $foo_page_updated and $bar_page ..."
 	fi
 }
 
@@ -21,10 +21,10 @@ http "httpbin.org/cookies/set?page=4" --session=./bar.json > /dev/null
 bar_page_updated=$(jq --raw-output '.cookies[] | select(.name=="page") | .value' ./bar.json)
 
 foobar2(){
-	if [ "$foo_page_updated" = "$bar_page_updated" ]; then
-		echo "[FAILED] Values should not be the same ..."
+	if [[ "$foo_page_updated" == 2 && "$bar_page_updated" == 4 ]]; then
+		echo "[PASSED] Correct values of updated foo and updated bar"
 	else
-		echo "[PASSED] Values of updated foo and updated bar are different"
+		echo "[FAILED] Incorrect values $foo_page_updated and $bar_page_updated ..."
 	fi
 }
 
