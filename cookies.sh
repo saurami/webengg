4e8f2763bffe749599a8148a662060e84d7ab377
saurami <106704404+saurami@users.noreply.github.com>
2022-06-26 11:25:41 -0700

    added shebang (debug 3)

--- cookies.sh
+++ cookies.sh
@@ -1,3 +1,5 @@
+#!/usr/bin/zsh
+
 http "httpbin.org/cookies/set?page=1" "Cookie:user=foo" --session=./foo.json > /dev/null
 foo_page=$(jq --raw-output '.cookies[] | select(.name=="page") | .value' ./foo.json)
 http "jsonplaceholder.typicode.com/posts?_page=$foo_page&_limit=25" > /dev/null
@@ -10,7 +12,7 @@ http "httpbin.org/cookies/set?page=3&user=bar" --session=./bar.json > /dev/null
 bar_page=$(jq --raw-output '.cookies[] | select(.name=="page") | .value' ./bar.json)
 
 foobar1(){
-	if [ "$foo_page_updated" == 2 && "$bar_page" == 3 ]; then
+	if [[ "$foo_page_updated" == 2 && "$bar_page" == 3 ]]; then
 		echo "[PASSED] Correct values of updated foo and initial bar"
 	else
 		echo "[FAILED] Incorrect values $foo_page_updated and $bar_page ..."
@@ -21,7 +23,7 @@ http "httpbin.org/cookies/set?page=4" --session=./bar.json > /dev/null
 bar_page_updated=$(jq --raw-output '.cookies[] | select(.name=="page") | .value' ./bar.json)
 
 foobar2(){
-	if [ "$foo_page_updated" == 2 && "$bar_page_updated" == 4 ]; then
+	if [[ "$foo_page_updated" == 2 && "$bar_page_updated" == 4 ]]; then
 		echo "[PASSED] Correct values of updated foo and updated bar"
 	else
 		echo "[FAILED] Incorrect values $foo_page_updated and $bar_page_updated ..."
